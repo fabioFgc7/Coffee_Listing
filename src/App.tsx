f0f3cd5cac7d4b2080730b8cdb2eae16ffd755a3
fabioFgc7 <99999716+fabioFgc7@users.noreply.github.com>
2023-11-14 18:32:50 -0300

    se agrego estilo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 function App() {
   return (
-    <main className='md:mt-[89px] w-full h-full flex justify-center items-center md:p-20 py-[107px] p-10 '>
+    <main className='md:mt-[89px] w-full h-full flex justify-center items-center md:p-[5.5rem] py-[7rem] p-10 '>
       <CoffeeHome />
     </main>
   );
